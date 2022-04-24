2b831064027be466c21bccfb843200c6a2e83019
pgm-kilidebo <kilian.debock@student.arteveldehs.be>
2022-04-24 14:47:47 +0200

    22.16.7.1: Testing

--- docs/static/js/app.js
+++ docs/static/js/app.js
@@ -5,10 +5,25 @@
       this.generateUI();
     },
     cacheElements() {
-      // this. = document.querySelectorAll('selector');
+      this.$bgLines = document.querySelectorAll('.bg-lines__line');
       // this. = document.querySelector('selector');
     },
     generateUI() {
+      this.changePos();
+      setInterval(() => this.changePos(), 20000);
+    },
+    changePos() {
+      this.$bgLines.forEach(line => {
+        const randomNr = Math.floor(Math.random() * 360);
+        const {randomHeight, randomWidth} = this.randomPosition();
+        line.style.transform = `translate(calc(${randomWidth}% - 50%), calc(${randomHeight}vh - 50%)) rotate(${randomNr}deg)`;
+      });
+    },
+    randomPosition() {
+      const randomHeight = Math.floor(Math.random() * 25);
+      const randomWidth = Math.floor(Math.random() * 25);
+
+      return {randomHeight, randomWidth};
     }
   };
   // Start initialization.

9c518e15923d99a6c0e5a7d5ab6f973a8caf962e
KilianDeBock <kilian.debock@student.arteveldehs.be>
2022-05-05 15:20:24 +0200

    Updates

---
+++ static/js/app.js
@@ -0,0 +1,14 @@
+(() => {
+  const app = {
+    initialize() {
+      this.cacheElements();
+      this.generateUI();
+    },
+    cacheElements() {
+    },
+    generateUI() {
+    },
+  };
+  // Start initialization.
+  app.initialize();
+})();

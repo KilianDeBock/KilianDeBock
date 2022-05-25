d64e1f406cb8f29894cf54003b7334c9213a3656
Stualyttle <Stualyttle@users.noreply.github.com>
2022-05-25 16:51:02 +0200

    22.20.7.4: fix urls

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,18 +8,14 @@ import BigBgLayout from "./components/layout/BigBgLayout";
 function App() {
   return (
     <div className="app">
-      <Routes basename="/KilianDeBock">
-        <Route exact path="/KilianDeBock" element={<BigBgLayout />}>
+      <Routes basename="/">
+        <Route exact path="/" element={<BigBgLayout />}>
           <Route index element={<HomePage />} />
         </Route>
-        <Route path="/KilianDeBock" element={<BaseLayout />}>
-          <Route exact path="/KilianDeBock/about" element={<AboutPage />} />
-          <Route
-            exact
-            path="/KilianDeBock/portfolio"
-            element={<PortfolioPage />}
-          />
-          <Route exact path="/KilianDeBock/contact" element={<ContactPage />} />
+        <Route path="/" element={<BaseLayout />}>
+          <Route exact path="/about" element={<AboutPage />} />
+          <Route exact path="/portfolio" element={<PortfolioPage />} />
+          <Route exact path="/contact" element={<ContactPage />} />
         </Route>
       </Routes>
     </div>

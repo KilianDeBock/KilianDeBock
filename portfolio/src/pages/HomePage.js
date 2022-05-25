d44cf73c3f9aed3ecf0cd2642dd98d9a6f8590f8
Stualyttle <Stualyttle@users.noreply.github.com>
2022-05-25 17:54:57 +0200

    22.20.7.8: Removed some spelling mistakes.

--- portfolio/src/pages/HomePage.js
+++ portfolio/src/pages/HomePage.js
@@ -69,15 +69,15 @@ const HomePage = () => {
         <article className="code code--fill code--text code--space">
           <span className="code__title">2013</span>
           I've had my first introduction to the world-famous game; Minecraft,
-          after a A few weeks I started my own server. First just to play with
-          my brothers, but eventually also to the public. We hit a total number
-          of ever played players of 3,356 players, and concurrent players are 23
-          at the same time. I used public plugins and asked a friend to also
-          write plugins for me.
+          after a few weeks I started my own server. First just to play with my
+          brothers, but eventually also to the public. We hit a total number of
+          ever played players of 3,356 players, and concurrent players are 23 at
+          the same time. I used public plugins and asked a friend to also write
+          plugins for me.
         </article>
         <article className="code code--fill code--text code--space">
           <span className="code__title">2017</span>
-          The server stopped on 2017 due to budget shortage. I myself started
+          The server stopped in 2017 due to budget shortage. I myself started
           working for a friend's server where I supervised staff members and did
           the distribution of tasks.
         </article>
@@ -86,17 +86,17 @@ const HomePage = () => {
           This was my first introduction to code. I needed a script to
           distribute plugins across different servers from one central location.
           This friend of mine said he'd be happy if we did it together and I
-          could learn from it. So I did so I could do it later on myself. So the
-          ball started rolling, I wrote my first bot in Python which has since
-          been implemented by 10 communities in Discord. I also code bots for
-          friends with specific community bots needs.
+          could learn from it. I started it so I could do it later by myself. So
+          the ball started rolling, I wrote my first bot in Python which has
+          since been implemented by 10 communities in Discord. I also code bots
+          for friends with specific community bots needs.
         </article>
         <article className="code code--fill code--text code--space">
           <span className="code__title">2021</span>
-          In 2021 the year I graduated high school from child care I decided to
-          do a 360 turn and start a higher education namely programming. This
+          In 2021, the year I graduated high school from child care I decided to
+          do a 360 turn and start a higher education named programming. This
           turned out to be a great success and something I really like to do.
-          Until now I am busy with the training and I am having the time of my
+          Until now I am busy with the course and I am having the time of my
           life!
         </article>
       </section>
